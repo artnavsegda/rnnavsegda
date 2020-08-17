f11f40f764afb969fba49cb5ab4022a35a7c0cc7
artnavsegda <artnavsegda@gmail.com>
2020-08-18 01:37:54 +0300

    state

--- artnavsegda/App.js
+++ artnavsegda/App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { StyleSheet, Text, View, Switch} from 'react-native';
+
+const initialState = {
+
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Switch value={true} />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
+
+const store = createStore(reducer);
 
 const styles = StyleSheet.create({
   container: {
