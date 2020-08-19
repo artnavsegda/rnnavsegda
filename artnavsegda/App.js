1da6bdba60aae31f13b2ab518fcefaf70d336746
artnavsegda <artnavsegda@gmail.com>
2020-08-19 15:45:47 +0300

    Update App.js

--- artnavsegda/App.js
+++ artnavsegda/App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { createStore } from "redux";
 import { connect, Provider } from "react-redux";
-import { StyleSheet, Text, View, Switch} from 'react-native';
+import { StyleSheet, Text, View, Switch, TouchableOpacity} from 'react-native';
+import styled from "styled-components";
 
 function mapStateToProps(state) {
   return { action: state.action }
@@ -15,6 +16,33 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   return state;
 }
+
+class Tile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lightOn: true
+    };
+  }
+  changeColor = () => {
+    this.setState({lightOn: !this.state.lightOn});
+  }
+  render() {
+    return(
+      <Container onPress={this.changeColor} style={ { backgroundColor: `${ this.state.lightOn ? "#f99" : "#9f9" }` } } >
+        <Text>{this.props.caption}</Text>
+        <Switch value={this.state.lightOn} onValueChange={this.changeColor} />
+      </Container>
+    )
+  }
+}
+
+const Container = styled.TouchableOpacity`
+  background: gray;
+  width: 100px;
+  height: 100px;
+  border-radius: 14px;
+`;
 
 class App extends Component {
   constructor(props) {
@@ -30,8 +58,8 @@ class App extends Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Text>Свет</Text>
-          <Switch value={this.state.lightOn} onValueChange={this.changeColor} />
+          <Tile caption="Потолок"/>
+          <Tile caption="Лампа"/>
           <StatusBar style="auto" />
         </View>
       </Provider>
