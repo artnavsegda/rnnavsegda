e4a0f302a311a469ee58cf456b759fa3d3ca5dc8
Art Navsegda <artnavsegda@gmail.com>
2020-08-24 17:00:43 +0300

    Update App.js

--- artnavsegda/App.js
+++ artnavsegda/App.js
@@ -74,3 +74,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+var ws = new WebSocket('ws://192.168.88.41:8080');
+
+ws.onopen = () => {
+  // connection opened
+  //ws.send('something'); // send a message
+  console.log("websock open");
+};
+
+ws.onmessage = (e) => {
+  // a message was received
+  console.log(e.data);
+};
+
+ws.onerror = (e) => {
+  // an error occurred
+  console.log(e.message);
+};
+
+ws.onclose = (e) => {
+  // connection closed
+  console.log(e.code, e.reason);
+};
