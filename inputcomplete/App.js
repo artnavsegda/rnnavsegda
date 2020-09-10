ccc897c08d17cb03b126f7e690c3736c42279f77
Art Navsegda <artnavsegda@gmail.com>
2020-09-10 12:30:51 +0300

    Update App.js

--- inputcomplete/App.js
+++ inputcomplete/App.js
@@ -1,22 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 export default function App() {
   const [timerID, setTimerId] = useState(0);
-  const [suggestions, setSuggestions] = useState("suggestions");
+  const [suggestions, setSuggestions] = useState("suggestions")
+  const [status, setStatus] = useState("ready")
 
   return (
     <View style={styles.container}>
       <Text>Enter something:</Text>
+      <Text>{status}</Text>
       <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
         onChangeText={askValue => {
           clearTimeout(timerID)
+          setStatus("loading")
           setTimerId(setTimeout(askValue => {
             fetch("https://artnavsegda.herokuapp.com/q?ask=" + askValue)
-            .then(response => response.json())
-            .then(variants => setSuggestions(variants.toString()));
+            .then(response => {
+              if (!response.ok)
+                throw new Error("connect failure")
+              return response.json()
+            })
+            .then(variants => setSuggestions(variants.toString()))
+            .catch(error => {
+              setStatus('error: ' + error);
+            })
           }, 1000, askValue))
         }}
       />
