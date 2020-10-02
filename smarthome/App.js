7b52f963ef844a12ba846a83e21cba3c65a5d6a8
Art Navsegda <artnavsegda@gmail.com>
2020-10-02 23:24:54 +0300

    grphql

--- smarthome/App.js
+++ smarthome/App.js
@@ -6,6 +6,7 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import gql from 'graphql-tag'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
@@ -15,6 +16,8 @@ const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
+
+const LIST_QUERY = 
 
 export default function App() {
   return (
