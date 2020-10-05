ab17b4af9a687de7d152c96cea7612baffe9d1da
Art Navsegda <artnavsegda@gmail.com>
2020-10-05 14:23:29 +0300

    Update App.js

--- smarthome/App.js
+++ smarthome/App.js
@@ -6,14 +6,33 @@ import { ApolloProvider, Query } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import gql from 'graphql-tag'
 
 const httpLink = createHttpLink({
   uri: 'http://192.168.88.23:4000'
 })
 
+const wsLink = new WebSocketLink({
+  uri: `ws://192.168.88.23:4000`,
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: link,
   cache: new InMemoryCache()
 })
 
@@ -27,13 +46,18 @@ const LIST_QUERY = gql`
   }
 `
 
+const UPDATE_SUBSCRIPTION = gql`
+
+`
+
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
         <Query query={LIST_QUERY} >
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
               if (loading || error) return <Text>loading</Text>
+
               return <FlatList
                 data={data.lights.map(light => {return {key: light.id, text: light.description, value: JSON.stringify(light.isOn)}})}
                 renderItem={({item}) => <Text>{item.text}: {item.value}</Text>}
