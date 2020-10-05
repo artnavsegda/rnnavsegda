d71241680fb44837a6ca8c1cf169b231263ae9b8
Art Navsegda <artnavsegda@gmail.com>
2020-10-05 15:48:17 +0300

    sorry baby

--- smarthome/App.js
+++ smarthome/App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
-
-import { ApolloProvider, Mutation, Query } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
+import { ApolloProvider, ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
 import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
 import { split } from 'apollo-link'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
-import gql from 'graphql-tag'
 
 const httpLink = createHttpLink({
   uri: 'http://192.168.88.23:4000'
@@ -48,7 +44,7 @@ const LIST_QUERY = gql`
 
 const TOGGLE_MUTATION = gql`
   mutation {
-    toggle(id: $id)
+    toggle(id: "light0")
     {
       id
       isOn
@@ -60,23 +56,22 @@ const TOGGLE_MUTATION = gql`
 //
 //`
 
+const LightsList = () => {
+  const { data, loading, error } = useQuery(LIST_QUERY);
+  if (loading || error) return <Text>loading</Text>
+  return (
+    <FlatList
+      data={data.lights.map(light => {return {key: light.id, text: light.description, value: JSON.stringify(light.isOn)}})}
+      renderItem={({item}) => <Text>{item.text}: {item.value}</Text>}
+    />
+  );
+}
+
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
-        <Query query={LIST_QUERY} >
-        {({ loading, error, data, subscribeToMore }) => {
-              if (loading || error) return <Text>loading</Text>
-
-              return <FlatList
-                data={data.lights.map(light => {return {key: light.id, text: light.description, value: JSON.stringify(light.isOn)}})}
-                renderItem={({item}) => <Text>{item.text}: {item.value}</Text>}
-              />
-          }}
-        </Query>
-        <Mutation mutation={TOGGLE_MUTATION} variables={{ id }}>
-        {postMutation  => <Button onPress={postMutation} title="Push"/>}
-        </Mutation>
+        <LightsList />
         <StatusBar style="auto" />
       </View>
     </ApolloProvider>
