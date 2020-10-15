39ed3c865df94739b5aa341402aab62c12459b31
Art Navsegda <artnavsegda@gmail.com>
2020-10-15 13:32:11 +0300

    Update App.js

--- carousel/App.js
+++ carousel/App.js
@@ -12,12 +12,12 @@ const ENTRIES1 = [
     {
         title: 'Beautiful and dramatic Antelope Canyon',
         subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-        illustration: require('../assets/store.jpg')
+        illustration: require('./assets/store.jpg')
     },
     {
         title: 'Earlier this morning, NYC',
         subtitle: 'Lorem ipsum dolor sit amet',
-        illustration: require('../assets/meeting.jpg')
+        illustration: require('./assets/meeting.jpg')
     }
 ];
 
