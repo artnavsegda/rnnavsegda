2287175727498ceacc85ca65b10d3a49c721f6da
Art Navsegda <artnavsegda@gmail.com>
2020-10-27 13:29:40 +0300

    podcasts

--- podcasts/App.tsx
+++ podcasts/App.tsx
@@ -3,14 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function PodcastList() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Подкасты" component={PodcastList} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
