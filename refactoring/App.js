b3c6b115122472461fa895bf9505f048f5f46c76
Art Navsegda <artnavsegda@gmail.com>
2020-11-06 14:32:41 +0300

    dependancies

--- refactoring/App.js
+++ refactoring/App.js
@@ -3,14 +3,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ApolloProvider } from "@apollo/client";
 import { client } from './Apollo'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
     </ApolloProvider>
   );
 }
