885759f48ffa192417199e4fd84195efcc643686
Art Navsegda <artnavsegda@gmail.com>
2020-11-06 14:43:01 +0300

    refactoring

--- refactoring/App.js
+++ refactoring/App.js
@@ -1,30 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ApolloProvider } from "@apollo/client";
-import { client } from './Apollo'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { client } from './Apollo'
+import HomeScreen from './screens/HomeScreen'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <NavigationContainer>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
+        <Stack.Navigator initialRouteName="Офис">
+          <Stack.Screen name="Офис" component={HomeScreen} options={{ headerShown: false }}/>
+        </Stack.Navigator>
       </NavigationContainer>
     </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
