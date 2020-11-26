dc886e8a5d1b3e150f7ebefa39ed01c622340c82
Art Navsegda <artnavsegda@gmail.com>
2020-11-26 22:06:30 +0300

    location app

--- vending/App.js
+++ vending/App.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-  const [value, onChangeText] = React.useState({
-    username: '',
-    password: ''
-  });
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Авторизация</Text>
-      <TextInput
-        placeholder="Логин"
-        style={styles.login}
-        onChangeText={text => onChangeText({...value, username: text})}
-        value={value.username}
-      />
-      <TextInput
-        placeholder="Пароль"
-        secureTextEntry
-        style={styles.login}
-        onChangeText={text => onChangeText({...value, password: text})}
-        value={value.password}
-      />
-      <Button title="Вход" />
+      <Text>{text}</Text>
     </View>
   );
 }
@@ -31,14 +35,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  login: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    margin: 5
-  }
 });
