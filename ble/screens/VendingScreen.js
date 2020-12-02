ea986e707fd2289fe3f4fb42bd47aa533cc0ccb0
Art Navsegda <artnavsegda@gmail.com>
2020-12-02 11:51:28 +0300

    Update VendingScreen.js

--- ble/screens/VendingScreen.js
+++ ble/screens/VendingScreen.js
@@ -1,28 +1,44 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux'
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, TouchableOpacity } from 'react-native';
 import MapView from 'react-native-maps';
 import styles from '../styles';
 import api from '../api.js';
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
+const Item = ({ title, onPress }) => (
+  <TouchableOpacity style={styles.item} onPress={onPress}>
     <Text style={styles.title}>{title}</Text>
-  </View>
+  </TouchableOpacity>
 );
 
 export default function VendingScreen() {
-    const token = useSelector(state => state.userToken)
+    const state = useSelector(state => state)
     const [data, setData] = React.useState({ isLoading: true, machines: [] });
+    const map = React.useRef(null);
+    const flatlist = React.useRef(null);
   
     React.useEffect(() => {
-       token ? fetch(api.machines, {headers: { token }})
+      state.userToken ? fetch(api.machines, {headers: { token: state.userToken }})
         .then(response => response.json())
-        .then(json => setData({isLoading: false, machines: json})) : undefined
+        .then(json => setData({isLoading: false, machines: json})) : null
     });
 
     const renderItem = ({ item }) => (
-      <Item title={item.Name} />
+      <Item title={item.Name} onPress={()=>{
+        console.log(item.Name)
+
+        map.current.animateCamera(
+          {
+            center: {
+              latitude: item.Latitude,
+              longitude: item.Longitude
+            },
+            zoom: 15
+          },
+          5000
+        );
+
+      }}/>
     );
   
     return (
@@ -32,19 +48,32 @@ export default function VendingScreen() {
         </View>
       ) : (
         <View style={{flex: 1}}>
-          <MapView style={{flex: 1}}>
+          <MapView style={{flex: 9}}
+            ref={map}
+            initialRegion={{
+              ...state.location.coords,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            showsUserLocation={true}
+            followsUserLocation={true}
+          >
             {data.machines.map((marker, index) => (
               <MapView.Marker
                 key={marker.GUID}
                 coordinate={{latitude: marker.Latitude, longitude: marker.Longitude}}
                 title={marker.Name}
                 description={marker.Address}
-                onPress={()=>{console.log(marker.GUID)}}
+                onPress={()=>{
+                  console.log(marker.GUID)
+                  flatlist.current.scrollToIndex({animated: true, index})
+                }}
               />
             ))}
           </MapView>
           <View style={{flex: 1}}>
             <FlatList
+              ref={flatlist}
               data={data.machines}
               renderItem={renderItem}
               keyExtractor={item => item.GUID}
