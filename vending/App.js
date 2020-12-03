c3b9216f00527572fc45bde8a2ba82a53e757b74
Art Navsegda <artnavsegda@gmail.com>
2020-12-03 17:48:05 +0300

    screens

--- vending/App.js
+++ vending/App.js
@@ -18,61 +18,7 @@ import SplashScreen from './screens/SplashScreen';
 import SignInScreen from './screens/SignInScreen';
 import VendingScreen from './screens/VendingScreen';
 import StorageScreen from './screens/StorageScreen';
-
-function ServiceScreen() {
-  const state = useSelector(state => state)
-  const [serviceState, setServiceState] = React.useState({stage: 0});
-
-  React.useEffect(() => {
-    let timerID = setInterval(()=>{
-      console.log("service " + state.servicingMachineID + " heartbeat");
-      fetch(api.status + '?' + new URLSearchParams({ MachineGUID: state.servicingMachineID }), {headers: { token: state.userToken }})
-      .then(response => response.json())
-      .then(status => {
-        console.log("status: " + JSON.stringify(status))
-        if (status.Door == 0)
-        {
-          clearInterval(timerID);
-          store.dispatch({ type: 'MACHINE', machine: null })
-        }
-      })
-    },5000)
-
-    
-
-  }, []);
-
-  switch (stage)
-  {
-    case 0:
-      return (
-        <View style={styles.container}>
-          <Text>Инвентаризация</Text>
-          <Button onPress={()=>{setServiceState({stage: 1})}}>Дальше</Button>
-        </View>
-      );
-    case 1:
-      return (
-        <View style={styles.container}>
-          <Text>Изъятие</Text>
-          <Button onPress={()=>{setServiceState({stage: 2})}}>Дальше</Button>
-        </View>
-      );
-    case 2:
-      return (
-        <View style={styles.container}>
-          <Text>Пополнение</Text>
-          <Button onPress={()=>{setServiceState({stage: 3})}}>Дальше</Button>
-        </View>
-      );
-    case 3:
-      return (
-        <View style={styles.container}>
-          <Text>Закройте дверь !</Text>
-        </View>
-      );
-  }
-}
+import ServiceScreen from './screens/ServiceScreen';
 
 function ProfileScreen() {
     return (
---
+++ vending/screens/ServiceScreen.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default function ServiceScreen() {
+    const state = useSelector(state => state)
+    const [serviceState, setServiceState] = React.useState({stage: 0});
+  
+    React.useEffect(() => {
+      let timerID = setInterval(()=>{
+        console.log("service " + state.servicingMachineID + " heartbeat");
+        fetch(api.status + '?' + new URLSearchParams({ MachineGUID: state.servicingMachineID }), {headers: { token: state.userToken }})
+        .then(response => response.json())
+        .then(status => {
+          console.log("status: " + JSON.stringify(status))
+          if (status.Door == 0)
+          {
+            clearInterval(timerID);
+            store.dispatch({ type: 'MACHINE', machine: null })
+          }
+        })
+      },5000)
+    }, []);
+  
+    switch (stage)
+    {
+      case 0:
+        return (
+          <View style={styles.container}>
+            <Text>Инвентаризация</Text>
+            <Button onPress={()=>{setServiceState({stage: 1})}}>Дальше</Button>
+          </View>
+        );
+      case 1:
+        return (
+          <View style={styles.container}>
+            <Text>Изъятие</Text>
+            <Button onPress={()=>{setServiceState({stage: 2})}}>Дальше</Button>
+          </View>
+        );
+      case 2:
+        return (
+          <View style={styles.container}>
+            <Text>Пополнение</Text>
+            <Button onPress={()=>{setServiceState({stage: 3})}}>Дальше</Button>
+          </View>
+        );
+      case 3:
+        return (
+          <View style={styles.container}>
+            <Text>Закройте дверь !</Text>
+          </View>
+        );
+    }
+  }
+  
