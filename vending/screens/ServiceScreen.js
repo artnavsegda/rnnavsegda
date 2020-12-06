4928758431e8a39545bfa2eeaaac431e1d770f82
Art Navsegda <artnavsegda@gmail.com>
2020-12-06 08:47:33 +0300

    Update ServiceScreen.js

--- vending/screens/ServiceScreen.js
+++ vending/screens/ServiceScreen.js
@@ -41,6 +41,7 @@ const Productlist = (props) => {
     />
     <Button onPress={()=>{
       props.onSend(state)
+      setState(props.data.map(element => {return {ProductID: element.ID, Quantity: 0}}))
     }}>Далее</Button>
   </View>
   );
@@ -58,7 +59,7 @@ export default function ServiceScreen({navigation}) {
       setProducts({loading: false, list: products})
     })
 
-/*     let timerID = setInterval(()=>{
+    let timerID = setInterval(()=>{
       console.log("service " + state.servicingMachineID + " heartbeat");
       fetch(api.status + '?' + new URLSearchParams({ MachineGUID: state.servicingMachineID }), {headers: { token: state.userToken }})
       .then(response => response.json())
@@ -70,7 +71,7 @@ export default function ServiceScreen({navigation}) {
           store.dispatch({ type: 'MACHINE', machine: null })
         }
       })
-    },5000) */
+    },5000)
   }, [])
 
   if (products.loading)
@@ -124,124 +125,3 @@ export default function ServiceScreen({navigation}) {
         )
   }
 }
-
-function ServiceScreen2() {
-    const state = useSelector(state => state)
-    const [localstate, localDispatch] = React.useReducer(
-      (prevState, action) => {
-        switch (action.type) {
-          case 'STAGE':
-            return {
-              ...prevState,
-              stage: action.stage
-            };
-          break;
-          case 'LOAD':
-            let blankdata = action.payload.map(element => {return {ProductID: element.ID, Quantity: 0}})
-            return {
-              ...prevState,
-              list: [
-                [...blankdata],
-                [...blankdata],
-                [...blankdata]
-              ]
-            }
-          break;
-          case 'SET':
-            let newState = {...prevState};
-            if (action.value >= 0)
-              newState.list[newState.stage][action.item].Quantity = action.value;
-            return newState;
-          break;
-          default:
-            return prevState;
-        }
-      }, {
-        stage: 0,
-        list: [
-          [],
-          [],
-          []
-        ]
-      }
-    );
-
-    const [products, setProducts] = React.useState({loading: true});
-  
-    React.useEffect(() => {
-        fetch(api.products + '?' + new URLSearchParams({ MachineGUID: state.servicingMachineID }), {headers: { token: state.userToken }})
-        .then(response => response.json())
-        .then(products => {
-          setProducts({loading: false, list: products})
-          localDispatch({ type: 'LOAD', payload: products });
-        })
-
-      let timerID = setInterval(()=>{
-        console.log("service " + state.servicingMachineID + " heartbeat");
-        fetch(api.status + '?' + new URLSearchParams({ MachineGUID: state.servicingMachineID }), {headers: { token: state.userToken }})
-        .then(response => response.json())
-        .then(status => {
-          console.log("status: " + JSON.stringify(status))
-          if (status.Door == 0)
-          {
-            clearInterval(timerID);
-            store.dispatch({ type: 'MACHINE', machine: null })
-          }
-        })
-      },5000)
-    }, []);
-
-    const renderItem = ({ item, index }) => (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Image style={{width: 60, height: 60, margin: 10, borderRadius: 10}} source={{uri: 'https://app.tseh85.com/DemoService/api/image?PictureId='+item.PictureID}}/>
-        <Paragraph style={{ flex: 4, textAlignVertical: 'center' }}>{item.Name}</Paragraph>
-        <Spinner 
-          value={localstate.list[localstate.stage][index] ? localstate.list[localstate.stage][index].Quantity : 0} 
-          onPlus={()=> localDispatch({ type: 'SET', item: index, value: localstate.list[localstate.stage][index].Quantity+1 })}
-          onMinus={()=> localDispatch({ type: 'SET', item: index, value: localstate.list[localstate.stage][index].Quantity-1 })}
-        />
-      </View>
-    );
-
-    if (products.loading)
-      return (
-        <View style={styles.container}>
-          <Text>Loading</Text>
-        </View>
-      );
-
-    switch (localstate.stage)
-    {
-      case 0:
-        return (
-          <View style={{flex: 1}}>
-            <Text>Инвентаризация</Text>
-            <FlatList data={products.list} renderItem={renderItem} keyExtractor={item => item.ID}/>
-            <Button onPress={()=>localDispatch({ type: 'STAGE', stage: 1 })}>Дальше</Button>
-          </View>
-        );
-      case 1:
-        return (
-          <View style={styles.container}>
-            <Text>Изъятие</Text>
-            <FlatList data={products.list} renderItem={renderItem} keyExtractor={item => item.ID}/>
-            <Button onPress={()=>localDispatch({ type: 'STAGE', stage: 2 })}>Дальше</Button>
-          </View>
-        );
-      case 2:
-        return (
-          <View style={styles.container}>
-            <Text>Пополнение</Text>
-            <FlatList data={products.list} renderItem={renderItem} keyExtractor={item => item.ID}/>
-            <Button onPress={()=>localDispatch({ type: 'STAGE', stage: 3 })}>Дальше</Button>
-          </View>
-        );
-      case 3:
-        return (
-          <View style={styles.container}>
-            <Text>Закройте дверь !</Text>
-          </View>
-        );
-    }
-  }
-  
