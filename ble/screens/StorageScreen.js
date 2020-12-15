5f70b7fc1f3c1079ee80c002515d7f0f654f97a7
Art Navsegda <artnavsegda@gmail.com>
2020-12-15 11:26:24 +0300

    Update StorageScreen.js

--- ble/screens/StorageScreen.js
+++ ble/screens/StorageScreen.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
-import { Text, View, Alert } from 'react-native';
-import { Button, Portal, Modal } from 'react-native-paper';
+import { View, Alert, Image, FlatList } from 'react-native';
+import { Button, Portal, Modal, Paragraph, Text, Title, Headline, Subheading } from 'react-native-paper';
 
 export default function StorageScreen() {
     const token = useSelector(state => state.userToken)
@@ -50,11 +50,24 @@ export default function StorageScreen() {
       setState({modalOpen: false, content:[], type: null});
     }
   
+    const renderItem = ({ item, index }) => (
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Image style={{width: 60, height: 60, margin: 10, borderRadius: 10}} source={{uri: 'https://app.tseh85.com/DemoService/api/image?PictureId='+item.PictureID}}/>
+        <Paragraph style={{ flex: 4, textAlignVertical: 'center' }}>{item.Name}</Paragraph>
+        <Paragraph style={{textAlignVertical: 'center', margin: 20 }}>{item.Quantity}</Paragraph>
+      </View>
+    );
+
     return (
       <View style={styles.container}>
         <Portal>
-          <Modal visible={state.modalOpen} dismissable={false} onDismiss={hideModal} contentContainerStyle={{backgroundColor: 'white', padding: 20, margin: 20, marginTop: 40 ,flex: 1}}>
-            <Text>{JSON.stringify(state.content)}</Text>
+          <Modal visible={state.modalOpen} dismissable={false} onDismiss={hideModal} contentContainerStyle={{backgroundColor: 'white', padding: 5, margin: 10, marginTop: 40 ,flex: 1}}>
+{/*             <Text>{JSON.stringify(state.content)}</Text> */}
+            <FlatList
+              data={state.content}
+              keyExtractor={item => item.ID}
+              renderItem={renderItem}
+            />
             <Button onPress={invoiceconfirm}>{state.type ? "Принять" : "Сдать"}</Button>
           </Modal>
         </Portal>
