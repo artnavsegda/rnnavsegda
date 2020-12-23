7a22bcaeb4e590715533b9ee38afbed98decab73
Art Navsegda <artnavsegda@gmail.com>
2020-12-23 19:25:30 +0300

    app

--- dark/App.js
+++ dark/App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
   );
 }
 
