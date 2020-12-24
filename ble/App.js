4162dce2f582a7763092585a9b687839b3b07116
Art Navsegda <artnavsegda@gmail.com>
2020-12-25 02:54:33 +0300

    Update App.js

--- ble/App.js
+++ ble/App.js
@@ -1,32 +1,59 @@
-import * as React from 'react';
+import * as React from 'react'
 import { Provider, useSelector } from 'react-redux'
-import { Text, View, PermissionsAndroid } from 'react-native';
-import { Provider as PaperProvider, Button } from 'react-native-paper';
-import { BleManager } from 'react-native-ble-plx';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Geolocation from '@react-native-community/geolocation';
+import { TextInput, View, Alert, useColorScheme } from 'react-native'
+import { 
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+  Button, Text, Appbar, Menu
+} from 'react-native-paper'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { 
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme
+} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
-import store from './store';
-import actions from './actions';
-import styles from './styles';
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
 
-import SplashScreen from './screens/SplashScreen';
-import SignInScreen from './screens/SignInScreen';
-import VendingScreen from './screens/VendingScreen';
-import StorageScreen from './screens/StorageScreen';
-import ServiceScreen from './screens/ServiceScreen';
+import store from './store'
+import actions from './actions'
+import styles from './styles'
 
-import manager from "./ble"
+import SplashScreen from './screens/SplashScreen'
+import SignInScreen from './screens/SignInScreen'
+import VendingScreen from './screens/VendingScreen'
+import StorageScreen from './screens/StorageScreen'
+import ServiceScreen from './screens/ServiceScreen'
 
-function ProfileScreen() {
+function CustomNavigationBar({ scene, navigation, previous }) {
+  const userToken = useSelector(state => state.userToken)
+  const { options } = scene.descriptor;
+  const title = options.headerTitle !== undefined
+    ? options.headerTitle
+    : options.title !== undefined
+    ? options.title
+    : scene.route.name;
+
+  const [visible, setVisible] = React.useState(false)
+  const openMenu = () => setVisible(true)
+  const closeMenu = () => setVisible(false)
+  
   return (
-    <View style={styles.container}>
-      <Button onPress={actions.signOut}>Выход</Button>
-    </View>
-  );
+    <Appbar.Header>
+      <Appbar.Content title={title} />
+      { userToken ? <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <Appbar.Action icon="menu" color="white" onPress={openMenu} />
+        }>
+        <Menu.Item onPress={actions.signOut} title="Выход" />
+      </Menu> : null }
+    </Appbar.Header>
+  )
 }
 
 const Tab = createMaterialTopTabNavigator();
@@ -36,7 +63,6 @@ function HomeScreen() {
     <Tab.Navigator>
       <Tab.Screen name="Аппараты" component={VendingScreen} />
       <Tab.Screen name="Склад" component={StorageScreen} />
-      <Tab.Screen name="Профиль" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
@@ -44,59 +70,58 @@ function HomeScreen() {
 const Stack = createStackNavigator();
 
 function App({ navigation }) {
+  const scheme = useColorScheme()
   const state = useSelector(state => state)
 
   React.useEffect(() => {
     const bootstrapAsync = async () => {
-      let userToken;
+      let userToken, userName;
 
       try {
-        userToken = await AsyncStorage.getItem('userToken');
-        userName = await AsyncStorage.getItem('userName');
+        userToken = await AsyncStorage.getItem('userToken')
+        userName = await AsyncStorage.getItem('userName')
       } catch (e) {
         // Restoring token failed
       }
-      store.dispatch({ type: 'RESTORE_TOKEN', token: userToken, username: userName });
+
+      store.dispatch({ type: 'RESTORE_TOKEN', token: userToken, username: userName })
     };
 
-    bootstrapAsync();
-
+    bootstrapAsync()
     PermissionsAndroid.requestMultiple([
       PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION, 
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
     ])
     .then(permissions => {
-      console.log(JSON.stringify(permissions) + "granted");
+      console.log(JSON.stringify(permissions) + "granted")
       function setloc(location)
       {
-        console.log(JSON.stringify(location))
-        store.dispatch({ type: 'LOCATION', location: location });
+        store.dispatch({ type: 'LOCATION', location: location })
       }
-      Geolocation.getCurrentPosition(setloc);
-      Geolocation.watchPosition(setloc);
+      Geolocation.getCurrentPosition(setloc)
+      Geolocation.watchPosition(setloc)
 
       const subscription = manager.onStateChange((state) => {
         if (state === 'PoweredOn') {
-            console.log("BLE ok");
-            subscription.remove();
+            subscription.remove()
         }
         else
-          console.log("BLE: " + JSON.stringify(state));
-      }, true);
+          console.log("BLE: " + JSON.stringify(state))
+      }, true)
     })
-  }, []);
+  }, [])
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer theme={scheme === 'dark' ? NavigationDarkTheme : NavigationDefaultTheme}>
+      <Stack.Navigator screenOptions={{ header: (props) => <CustomNavigationBar {...props} /> }}>
         {state.isLoading ? (
-          <Stack.Screen name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Загрузка..." component={SplashScreen} />
         ) : state.userToken == null ? (
           <Stack.Screen
             name="SignIn"
             component={SignInScreen}
             options={{
-              title: 'Холодильник МиниЦЕХ',
+              title: 'Цех 85',
               animationTypeForReplace: state.isSignout ? 'pop' : 'push',
             }}
           />
@@ -111,6 +136,13 @@ function App({ navigation }) {
 }
 
 export default function ConnectedApp() {
+  const [permission, askForPermission] = Permissions.usePermissions(Permissions.LOCATION, { ask: true });
+
+  Location.watchPositionAsync({}, (location) => {
+    store.dispatch({ type: 'LOCATION', location: location })
+    console.log(JSON.stringify(location))
+  })
+
   return(
     <Provider store={store}>
       <PaperProvider>
