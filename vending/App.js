cffea95b77cee6f580dd2c98e0775a66a5f035ab
Art Navsegda <artnavsegda@gmail.com>
2020-12-24 08:54:09 +0300

    Update App.js

--- vending/App.js
+++ vending/App.js
@@ -8,7 +8,11 @@ import {
   Button, Text, Appbar, Menu
 } from 'react-native-paper'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { NavigationContainer } from '@react-navigation/native'
+import { 
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme
+} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
