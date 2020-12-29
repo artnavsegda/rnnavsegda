115d60c748120eb8f6d2c9624bf92d93329c52ac
Art Navsegda <artnavsegda@gmail.com>
2020-12-29 11:17:49 +0300

    tamtam

--- rnn/index.js
+++ rnn/index.js
@@ -1,22 +1,50 @@
-/**
- * @format
- */
+// In index.js of a new project
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Navigation } from 'react-native-navigation';
 
-import { Navigation } from "react-native-navigation";
-import App from './App';
-Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
-Navigation.events().registerAppLaunchedListener(() => {
-   Navigation.setRoot({
-     root: {
-       stack: {
-         children: [
-           {
-             component: {
-               name: 'com.myApp.WelcomeScreen'
-             }
-           }
-         ]
-       }
-     }
+const HomeScreen = (props) => {
+  return (
+    <View style={styles.root}>
+      <Text>Hello React Native Navigation 👋</Text>
+    </View>
+  );
+};
+HomeScreen.options = {
+  topBar: {
+    title: {
+      text: 'Home',
+      color: 'white'
+    },
+    background: {
+      color: '#4d089a'
+    }
+  }
+}
+
+Navigation.registerComponent('Home', () => HomeScreen);
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home'
+            }
+          }
+        ]
+      }
+    }
   });
+});
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke'
+  }
 });
