c8dfeb166670e6db899c04e3ef1b03d6368a5d72
Art Navsegda <artnavsegda@gmail.com>
2021-01-06 23:34:23 +0300

    -ble

--- bareble/App.js
+++ bareble/App.js
@@ -21,7 +21,6 @@ import * as Location from 'expo-location'
 import store from './store'
 import actions from './actions'
 import styles from './styles'
-import manager from './ble'
 
 import CustomNavigationBar from './components/NavBar'
 
@@ -30,6 +29,7 @@ import SignInScreen from './screens/SignInScreen'
 import VendingScreen from './screens/VendingScreen'
 import StorageScreen from './screens/StorageScreen'
 import ServiceScreen from './screens/ServiceScreen'
+import BLEScanner from './screens/BleScreen'
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -39,62 +39,6 @@ function HomeScreen() {
       <Tab.Screen name="Аппараты" component={VendingScreen} />
       <Tab.Screen name="Склад" component={StorageScreen} />
     </Tab.Navigator>
-  );
-}
-
-function BLEScanner() {
-  const [myMap, setMyMap] = React.useState(new Map());
-  const [scanning, setScanning] = React.useState(false);
-
-  function scan_stop()
-  {
-    setScanning(false);
-    manager.stopDeviceScan()
-  }
-
-  function scan_start()
-  {
-    scan_stop();
-    setScanning(true);
-    manager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.error(error)
-        return
-      }
-      console.log("Found: " + device.name + "id: " +  device.id)
-      setMyMap(new Map(myMap.set(device.id, {name: device.name, lastSeen: Date.now()})))
-    });
-
-    setTimeout(()=>{
-      scan_stop();
-    },60000)
-  }
-
-  return (
-    <View>
-      <View style={{flexDirection: "row"}}>
-      <Button onPress={scan_start}>Start scan</Button>
-      <Button onPress={scan_stop}>Stop scan</Button>
-      <ActivityIndicator animating={scanning} />
-      </View>
-      <DataTable>
-      <DataTable.Header>
-        <DataTable.Title>MAC</DataTable.Title>
-        <DataTable.Title>Name</DataTable.Title>
-        <DataTable.Title>Date</DataTable.Title>
-      </DataTable.Header>
-      {[...myMap.keys()].map(k => {
-        let d = new Date(myMap.get(k).lastSeen);
-        return (
-          <DataTable.Row key={k}>
-            <DataTable.Cell>{k}</DataTable.Cell>
-            <DataTable.Cell>{myMap.get(k).name}</DataTable.Cell>
-            <DataTable.Cell>{d.getHours()}:{d.getMinutes()}:{d.getSeconds()}</DataTable.Cell>
-          </DataTable.Row>
-          )
-      })}
-      </DataTable>
-    </View>
   );
 }
 
---
+++ bareble/screens/BleScreen.js
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import { View } from 'react-native'
+import { Button, ActivityIndicator, DataTable } from 'react-native-paper'
+import manager from '../ble'
+
+export default function BLEScanner() {
+    const [myMap, setMyMap] = React.useState(new Map())
+    const [scanning, setScanning] = React.useState(false)
+  
+    function scan_stop()
+    {
+      setScanning(false)
+      manager.stopDeviceScan()
+    }
+  
+    function scan_start()
+    {
+      scan_stop();
+      setScanning(true);
+      manager.startDeviceScan(null, null, (error, device) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+        console.log("Found: " + device.name + "id: " +  device.id)
+        setMyMap(new Map(myMap.set(device.id, {name: device.name, lastSeen: Date.now()})))
+      });
+  
+      setTimeout(()=>{
+        scan_stop()
+      },60000)
+    }
+  
+    return (
+      <View>
+        <View style={{flexDirection: "row"}}>
+        <Button onPress={scan_start}>Start scan</Button>
+        <Button onPress={scan_stop}>Stop scan</Button>
+        <ActivityIndicator animating={scanning} />
+        </View>
+        <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>MAC</DataTable.Title>
+          <DataTable.Title>Name</DataTable.Title>
+          <DataTable.Title>Date</DataTable.Title>
+        </DataTable.Header>
+        {[...myMap.keys()].map(k => {
+          let d = new Date(myMap.get(k).lastSeen);
+          return (
+            <DataTable.Row key={k}>
+              <DataTable.Cell>{k}</DataTable.Cell>
+              <DataTable.Cell>{myMap.get(k).name}</DataTable.Cell>
+              <DataTable.Cell>{d.getHours()}:{d.getMinutes()}:{d.getSeconds()}</DataTable.Cell>
+            </DataTable.Row>
+            )
+        })}
+        </DataTable>
+      </View>
+    );
+  }
