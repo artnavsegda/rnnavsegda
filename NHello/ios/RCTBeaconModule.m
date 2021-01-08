d2a651947a91285e7e5880dfa837e2367215f6f1
Art Navsegda <artnavsegda@gmail.com>
2021-01-08 03:10:33 +0300

    module

---
+++ NHello/ios/RCTBeaconModule.m
@@ -0,0 +1,16 @@
+//
+//  RCTBeaconModule.m
+//  NHello
+//
+//  Created by Art Navsegda on 08.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBeaconModule.h"
+
+@implementation RCTBeaconModule
+
+// To export a module named RCTCalendarModule
+RCT_EXPORT_MODULE();
+
+@end
