75147c6939ab31c8f11151141132f7d088658c8d
Art Navsegda <artnavsegda@gmail.com>
2021-01-08 06:37:05 +0300

    Update App.js

--- barenative/App.js
+++ barenative/App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeModules, StyleSheet, Text, View } from 'react-native';
+
+const { BeaconModule } = NativeModules;
+
+const MyNativeComp = () => {
+  const onPress = () => {
+    console.log('We will invoke the native module here!');
+    BeaconModule.doSomething(
+      'testName',
+      'testLocation',
+      (eventId) => {
+        console.log(`eventid is ${eventId}`);
+      }
+    );
+    console.log("return: " + BeaconModule.getName());
+  };
+
+  return (
+    <Button
+      title="Click to invoke your native module!"
+      color="#841584"
+      onPress={onPress}
+    />
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <MyNativeComp />
       <StatusBar style="auto" />
     </View>
   );
