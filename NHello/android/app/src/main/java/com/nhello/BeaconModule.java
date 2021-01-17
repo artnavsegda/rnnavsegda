03e0aefd154c13b55bf96c172a5a96572eb945b4
Art Navsegda <artnavsegda@gmail.com>
2021-01-17 15:57:19 +0300

    Update BeaconModule.java

--- NHello/android/app/src/main/java/com/nhello/BeaconModule.java
+++ NHello/android/app/src/main/java/com/nhello/BeaconModule.java
@@ -4,13 +4,21 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import android.util.Log;
 
 public class BeaconModule extends ReactContextBaseJavaModule {
+    BeaconModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
     public String getName() {
         return "BeaconModule";
     }
 
-    BeaconModule(ReactApplicationContext context) {
-        super(context);
+    @ReactMethod
+    public void createCalendarEvent(String name, String location) {
+        Log.d("CalendarModule", "Create event called with name: " + name
+                + " and location: " + location);
     }
 }
