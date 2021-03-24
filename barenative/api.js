d5de8b5903c2191361bc23e102f96084d9649f01
Art Navsegda <artnavsegda@gmail.com>
2021-03-24 21:31:26 +0300

    image

--- barenative/api.js
+++ barenative/api.js
@@ -1,6 +1,7 @@
 const API_PATH = 'https://app.tseh85.com/DemoService/api';
 
 export default api = {
+  image: API_PATH + '/image',
   auth: API_PATH + '/AuthenticateVending',
   machines: API_PATH + '/vending/machines',
   openlock: API_PATH + '/vending/openlock',
