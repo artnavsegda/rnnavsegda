ed808b6d428babbd1aa96723c5d045819d02fcf4
Art Navsegda <artnavsegda@gmail.com>
2021-04-19 21:56:09 +0300

    Update App.js

--- image-upload/App.js
+++ image-upload/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Modal, TouchableNativeFeedback, Text, Image } from 'react-native';
-import ImageViewer from 'react-native-image-zoom-viewer';
+import { View, Modal, TouchableNativeFeedback, Text, Image, Dimensions } from 'react-native';
+// import ImageViewer from 'react-native-image-zoom-viewer';
+import ImageZoom from 'react-native-image-pan-zoom';
 
 const images = [
   {
@@ -23,7 +24,25 @@ export default class Main extends Component {
     return (
       <View style={{ padding: 10 }} >
         <Text>Test</Text>
-        <Image 
+        <ImageZoom cropWidth={Dimensions.get('window').width}
+                       cropHeight={Dimensions.get('window').height}
+                       imageWidth={1024}
+                       imageHeight={768}
+                       enableCenterFocus={false}
+                       >
+                <Image 
+                  style={{
+                    width: 1024,
+                    height: 768,
+                  }}
+                  onLoad={(event) => console.log(event.nativeEvent)}
+                  source={{
+                      uri: "https://app.tseh85.com/DemoService/api/v21/support/image?SupportId=43251&IsPreview=false",
+                      headers: {Token: "B8ZSXfrbpk5Qc/9oTf+KZHx6hgf5MBL8edwyYvA2VFypvgntWlgmxS9NB7mfysJncp+JoScu0B3u+92ioqvfSQ=="}
+                  }}
+                />
+        </ImageZoom>
+{/*         <Image 
           style={{width:50, height:50}}
           onLoad={(event) => console.log(event.nativeEvent)}
           source={{
@@ -37,7 +56,7 @@ export default class Main extends Component {
           source={{
               uri: "https://avatars2.githubusercontent.com/u/7970947?v=3&s=460",
           }}
-        />
+        /> */}
 {/*         <Modal
           visible={this.state.modalVisible}
           transparent={true}
