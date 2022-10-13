b5a70618d9453ab9533f2e8738e413471464a19a
Art Navsegda <artnavsegda@gmail.com>
2022-10-13 15:53:37 +0300

    a

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
   });
 
   const setMyPos = async () => {
-    requestMultiple([
+    await requestMultiple([
       Platform.OS == 'ios' ? PERMISSIONS.IOS.LOCATION_ALWAYS : PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
       Platform.OS == 'ios' ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
     ])
