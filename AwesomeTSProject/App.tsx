28b23e0259243858f73558d25f415c3372a17558
Art Navsegda <artnavsegda@gmail.com>
2022-10-17 08:03:52 +0300

    Update App.tsx

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -12,15 +12,27 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { NavigationFunctionComponent } from 'react-native-navigation';
+import { Navigation, NavigationFunctionComponent } from 'react-native-navigation';
 
 
-const App: NavigationFunctionComponent = () => {
-
+const App: NavigationFunctionComponent = (props) => {
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView style={{ position: "absolute" }}>
-        <Button title='a' onPress={() => { }} />
+        <Button title='a' onPress={() => {
+          Navigation.push(props.componentId, {
+            component: {
+              name: 'Settings', // Push the screen registered with the 'Settings' key
+              options: { // Optional options object to configure the screen
+                topBar: {
+                  title: {
+                    text: 'Settings' // Set the TopBar title of the new Screen
+                  }
+                }
+              }
+            }
+          });
+        }} />
         <Text></Text>
         <Text></Text>
         <Icon name="gps-fixed" size={30} />
