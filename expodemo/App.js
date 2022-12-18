4ef7a5baf995c190fe61308ed032f0b57005d112
Art Navsegda <artnavsegda@gmail.com>
2022-12-18 13:03:28 +0300

    Update App.js

--- expodemo/App.js
+++ expodemo/App.js
@@ -3,15 +3,19 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#dff' }}>
-      <View style={{
-        flex: 1,
-        backgroundColor: '#fdf'
-      }}>
-        <View style={{ backgroundColor: '#ff0', position: 'absolute', left: 10, top: 10 }}>
-          <Text>Hello</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, backgroundColor: '#f0f' }} />
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{
+          flex: 1,
+          backgroundColor: '#0ff'
+        }}>
+          <View style={{ backgroundColor: '#ddf', flex: 1 }} />
+          <View style={{ backgroundColor: '#ff0', position: 'absolute', left: 10, top: 10 }}>
+            <Text>Hello</Text>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </View>
   );
 }
